eb07593f43d08c7c4d64d00cdec0a4cf7491a80a
dbenarfa <dbenarfa@outlook.com>
2017-03-14 15:47:49 +0100

    Added variables declaration

--- num2let.py
+++ num2let.py
@@ -6,6 +6,45 @@ __create_date__ = "12/03/2017"
 __version__ = "0.1"
 __email__ = "dbenarfa@outlook.com"
 
+numbers_less_17 = {1: "un",
+                       2: "deux",
+                       3: "trois",
+                       4: "quatre",
+                       5: "cinq",
+                       6: "six",
+                       7: "sept",
+                       8: "huit",
+                       9: "neuf",
+                       0: "zero",
+                       10: "dix",
+                       11: "onze",
+                       12: "douze",
+                       13: "treize",
+                       14: "quatorze",
+                       15: "quinze",
+                       16: "seize"
+                       }
+dix = {1: "dix",
+       2: "vingt",
+       3: "trente",
+       4: "quarante",
+       5: "cinquante",
+       6: "soixante",
+       7: "soixante-dix",
+       8: "quatre-vingt",
+       9: "quatre-vingt-dix"
+       }
+
+cent_label = "cent"
+mille_label = "mille"
+million_label = "million"
+milliard_label = "milliard"
+billion_label = "billion"
+billiard_label = "billiard"
+far_number = "ou allez vous.."
+plus = "s"
+vergule_label = "vergule"
+et_label = "et"
 
 
 def split_number(number):
